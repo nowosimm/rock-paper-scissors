2ec6ffbae773e29d4d094c069c0c605ed53f67aa
Maddie <nowosimm@mail.uc.edu>
2023-08-31 11:08:58 -0400

    event listeners added for click buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,79 +1,98 @@
-
-let shouldPlay = prompt("Do you want to play rock, paper, scissors?")
-
 let userScore = 0
 let computerScore = 0
 let gameNumber = 0
 
+const rockButton = document.createElement('button');
+rockButton.textContent = "Rock"
+rockButton.onclick = playRound;
+document.body.appendChild(rockButton);
 
-if (shouldPlay.toLowerCase() == "yes") {
-    while (true) {
+const paperButton = document.createElement('button');
+paperButton.textContent = "Paper"
+paperButton.onclick = playRound;
 
-        gameNumber = gameNumber + 1
+document.body.appendChild(paperButton);
+
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = "Scissors"
+scissorsButton.onclick = playRound;
+
+document.body.appendChild(scissorsButton);
 
 
-        let userChoice = prompt('Pick rock, paper, or scissors!')
-        userChoice = userChoice.toLowerCase()
+// let shouldPlay = prompt("Do you want to play rock, paper, scissors?")
+// if (shouldPlay.toLowerCase() == "asdfsd") {
+//     while (true) {
 
-        let computerChoice = getComputerChoice()
+//         playRound();
 
-        if (computerChoice == userChoice) {
-            alert("Its a tie!")
-        }
+//         let playAgain = prompt("Do you want to play again?")
+//         if (playAgain.toLowerCase() == "no") {
+//             break;
+//         }
 
-        if (userChoice == "rock" && computerChoice == "paper") {
-            alert("computer chose paper, you lose :(")
-        }
+//         userScore = 0
+//         computerScore = 0
+//         gameNumber = 0
 
-        if (userChoice == "rock" && computerChoice == "scissors") {
-            alert("computer chose scissors, you win!")
-            userScore = userScore + 1
-        }
+//     }
 
-        if (userChoice == "paper" && computerChoice == "scissors") {
-            alert("computer chose scissors, you lose :(")
-        }
+// } else {
+//     alert("you dont wont to play")
+// }
 
-        if (userChoice == "paper" && computerChoice == "rock") {
-            alert("computer chose rock, you win!")
-            userScore = userScore + 1
-        }
+function playRound() {
+    gameNumber = gameNumber + 1
 
-        if (userChoice == "scissors" && computerChoice == "rock") {
-            alert("computer chose rock, you lose :(")
-        }
+    let userChoice = prompt('Pick rock, paper, or scissors!')
+    userChoice = userChoice.toLowerCase()
 
-        if (userChoice == "scissors" && computerChoice == "paper") {
-            alert("computer chose paper, you win!")
-        }
+    let computerChoice = getComputerChoice()
 
-        if (gameNumber == 5) {
-            alert("Your score is " + userScore + "!")
-
-            let playAgain = prompt("Do you want to play again?")
-            if (playAgain.toLowerCase() == "no") {
-                break;
-            }
-
-            userScore = 0
-            computerScore = 0
-            gameNumber = 0
-            
-        }
-      
+    if (computerChoice == userChoice) {
+        alert("Its a tie!")
     }
+
+    if (userChoice == "rock" && computerChoice == "paper") {
+        alert("computer chose paper, you lose :(")
+    }
+
+    if (userChoice == "rock" && computerChoice == "scissors") {
+        alert("computer chose scissors, you win!")
+        userScore = userScore + 1
+    }
+
+    if (userChoice == "paper" && computerChoice == "scissors") {
+        alert("computer chose scissors, you lose :(")
+    }
+
+    if (userChoice == "paper" && computerChoice == "rock") {
+        alert("computer chose rock, you win!")
+        userScore = userScore + 1
+    }
+
+    if (userChoice == "scissors" && computerChoice == "rock") {
+        alert("computer chose rock, you lose :(")
+    }
+
+    if (userChoice == "scissors" && computerChoice == "paper") {
+        alert("computer chose paper, you win!")
+    }
+
+    alert("Your score is " + userScore + "!")
 }
 
-    function getComputerChoice() {
-        // randomNumber can be (0, 1, 2)
-        let randomNumber = Math.floor(Math.random() * 3);
-        if (randomNumber == 0) {
-            return 'rock';
-        }
-        if (randomNumber == 1) {
-            return 'paper';
-        }
-        if (randomNumber == 2) {
-            return 'scissors'
-        }
+
+function getComputerChoice() {
+    // randomNumber can be (0, 1, 2)
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        return 'rock';
     }
+    if (randomNumber == 1) {
+        return 'paper';
+    }
+    if (randomNumber == 2) {
+        return 'scissors'
+    }
+}
